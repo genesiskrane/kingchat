b69c183d86119cd786744899d92f6ec6e2a5f3db
Augment Chinedu <augmentchinedu@gmail.com>
2024-07-07 04:06:55 +0100

    Side Navigation Added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,37 +11,39 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> King Chat </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <div class="column justify-between fit">
+      <div class="flex flex-col justify-between fit">
         <div>
-          <div class="row q-px-md q-py-lg bg-red-5">
+          <div class="flex q-px-md q-py-lg bg-red-5">
             <div>
-              <q-avatar size="6rem">
-                <img src="../assets/img/logo.png" />
-              </q-avatar>
+              <q-img
+                src="../assets/img/logo.png"
+                class="w-20 h-20 rounded-full"
+              />
             </div>
-            <div class="items-center q-mx-md w-12" style="display: grid;">
+            <div class="items-center q-mx-md grid">
               <span class="">King Chat</span>
             </div>
           </div>
-          <q-list>
-            <q-item-label header> Essential Links </q-item-label>
 
-            <EssentialLink
-              v-for="link in essentialLinks"
-              :key="link.title"
-              v-bind="link"
-            />
-          </q-list>
+          <navigation class="py-4"></navigation>
         </div>
-        <div id="footer">
-          <p>Footer</p>
+        <div id="footer" class="flex p-4 gap-4">
+          <div>
+            <q-avatar>
+              <img src="../assets/img/avatar.png" />
+            </q-avatar>
+          </div>
+          <div class="flex flex-col">
+            <div>Lord God</div>
+            <div>@_augment</div>
+          </div>
         </div>
       </div>
     </q-drawer>
@@ -53,71 +55,11 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
-
-export default defineComponent({
+export default {
   name: "MainLayout",
+};
+</script>
 
-  components: {
-    EssentialLink,
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
-  },
-});
+<script setup>
+import Navigation from "../components/Navigation.vue";
 </script>
