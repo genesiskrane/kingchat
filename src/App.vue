cc1b326323a0ded2a6665154996ec5e3a2c8b39c
Augment Chinedu <augmentchinedu@gmail.com>
2024-07-07 12:38:22 +0100

    Tailwind Installed. Navigation Compenent Made. New Vuetify setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <Navigation />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Components
+import App from './App.vue'
+import './assets/css/main.css'
+
+// Composables
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
