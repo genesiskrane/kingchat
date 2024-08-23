d77e33628830e9ca0a9248c1b47361bedb496fe8
Augment Chinedu <augmentchinedu@gmail.com>
2024-08-23 10:08:05 +0100

    Back Button Added to Page and tabs

--- src/pages/Page.vue
+++ src/pages/Page.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="page" class="w-full min-h-screen bg-black">
-    <div class="flex flex-col">
+    <div class="flex flex-col relative">
       <div id="cover" class="flex items-center justify-center w-full h-40 bg-blue-500 text-center">
         Cover
+      </div>
+      <div class="absolute flex flex-row justify-between w-full h-12 px-3 py-4">
+        <div>
+          <v-icon
+            icon="mdi-keyboard-backspace p-4"
+            class="bg-[#0000000a] rounded-full"
+            @click="back"
+          ></v-icon>
+        </div>
+        <div class="flex flex-row gap-2">
+          <v-icon icon="mdi-magnify" class="bg-[#0000000a] rounded-full p-4"></v-icon>
+          <v-icon icon="mdi-dots-vertical" class="bg-[#0000000a] rounded-full p-4"></v-icon>
+        </div>
       </div>
       <div id="profile" class="px-3">
         <div class="flex flex-row justify-between relative">
@@ -52,12 +65,20 @@
           <v-tab :value="2"> <v-icon icon="mdi-animation-play" size="large"></v-icon></v-tab>
           <v-tab :value="3"> <v-icon icon="mdi-card-account-details" size="large"></v-icon></v-tab>
         </v-tabs>
-        <v-tabs-window v-model="tab">
-          <v-tabs-window-item :value="1"> One </v-tabs-window-item>
+        <v-tabs-window v-model="tab" class="py-1.5">
+          <v-tabs-window-item :value="1" class="grid grid-cols-3 items-center gap-0.5">
+            <div v-for="(post, i) in page.posts" :key="i">
+              <v-img :src="profile.photoURL"></v-img>
+            </div>
+          </v-tabs-window-item>
 
-          <v-tabs-window-item :value="2"> Two </v-tabs-window-item>
+          <v-tabs-window-item :value="2" class="flex items-center justify-center">
+            Media Posts
+          </v-tabs-window-item>
 
-          <v-tabs-window-item :value="3"> Three </v-tabs-window-item>
+          <v-tabs-window-item :value="3" class="flex items-center justify-center">
+            Mentions
+          </v-tabs-window-item>
         </v-tabs-window>
       </div>
     </div>
@@ -65,9 +86,11 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, inject } from 'vue';
 import { useRoute } from 'vue-router';
 import { useAppStore } from '../stores';
+
+const { back } = inject('app');
 
 const store = useAppStore();
 const route = useRoute();
@@ -75,11 +98,11 @@ const username = route.params.username;
 const profile = ref({});
 const page = ref({});
 
-const tab = ref(null);
+const tab = ref(2);
 onMounted(async () => {
   const data = await store.getUserPage(username);
   profile.value = data.profile;
   delete data.profile;
-  page.value = data;
+  page.value = { posts: [{ name: 'Ade' }, {}, {}, {}] };
 });
 </script>
