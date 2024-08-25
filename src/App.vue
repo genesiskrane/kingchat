10cdf297a008eaeb1cd6e7e127102df895c3eb26
Augment Chinedu <augmentchinedu@gmail.com>
2024-08-26 00:37:52 +0100

    Shared Post Interface Updated

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <router-view></router-view>
-
 </template>
 
 <script setup>
-import { provide } from 'vue'
-import { useRouter } from 'vue-router'
+import { provide } from 'vue';
+import { useRouter } from 'vue-router';
 
-const router = useRouter()
+const router = useRouter();
 
 function back() {
-  if (window.history.length > 1) router.back()
-  else router.push({ path: '/home' })
+  if (window.history.length > 1) router.back();
+  else router.push({ path: '/home' });
+}
+
+function timeAgo(timestamp) {
+  const now = Date.now();
+  const diffInMilliseconds = now - timestamp;
+
+  const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
+  const diffInMinutes = Math.floor(diffInMilliseconds / (1000 * 60));
+  const diffInHours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
+  const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+  const diffInMonths = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 30));
+  const diffInYears = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 365));
+
+  if (diffInYears > 0) {
+    return `${diffInYears} year(s) ago`;
+  } else if (diffInMonths > 0) {
+    return `${diffInMonths} month(s) ago`;
+  } else if (diffInDays > 0) {
+    return `${diffInDays} day(s) ago`;
+  } else if (diffInHours > 0) {
+    return `${diffInHours} hour(s) ago`;
+  } else if (diffInMinutes > 0) {
+    return `${diffInMinutes} minute(s) ago`;
+  } else {
+    return `${diffInSeconds} second(s) ago`;
+  }
 }
 
 provide('app', {
-  back
-})
+  back,
+  timeAgo
+});
 </script>
-
-
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <div class="flex flex-col">
-      <div v-for="(post, i) in posts" :key="i">
-        {{ post.author.displayName }} <br />
-        {{ post.text }}
-
+    <div class="flex flex-col px-2 py-2">
+      <div v-for="(post, i) in posts" :key="i" class="clearfix">
+        <div class="py-2">
+          <v-img
+            :src="post.author.photoURL"
+            :alt="post.author.displayName + '\'s DP'"
+            class="w-12 h-12 rounded-full float-left m-1"
+          ></v-img>
+          <div>
+            <span class="font-semibold">{{ post.author.displayName }}</span>
+            &nbsp;
+            <span class="text-xs">@{{ post.author.username }} {{ timeAgo(post.time) }}</span>
+          </div>
+          <p>
+            {{ post.text }}
+          </p>
+        </div>
         <hr />
       </div>
     </div>
@@ -14,9 +26,11 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 import { useAppStore } from '../stores';
 import SpeedDial from '../components/ui/SpeedDial.vue';
+
+const { timeAgo } = inject('app');
 
 const store = useAppStore();
 const posts = computed(() => store.posts);
